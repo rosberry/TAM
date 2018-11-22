e79de4d497b785a5b1efa2636e22cdde7ed3d4db
Alexey Korshun <lehakorshun@gmail.com>
2018-11-22 12:32:34 +0600

    add sample

---
+++ sample/src/main/java/com/rosberry/android/tam/sample/MainActivity.kt
@@ -0,0 +1,18 @@
+/*
+ *
+ *  * Copyright (c) 2018 Rosberry. All rights reserved.
+ *
+ */
+
+package com.rosberry.android.tam.sample
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
